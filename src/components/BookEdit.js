272923113b3de1176f7fab204cec3cb3679f5b3e
Julian Jasionowski <julianpj1997@gmail.com>
2024-01-29 19:16:55 -0500

    Push4

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,27 @@
-function BookEdit() {
-    return <div>BookEdit</div>;
+import { useState } from 'react';
+
+function BookEdit({ book, onSubmit }) {
+    const [title, setTitle] = useState(book.title);
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        onSubmit(book.id, title);
+    };
+
+    return ( 
+        <form onSubmit={handleSubmit} className="book-edit">
+            <label>Title</label>
+            <input className="input" value={title} onChange={handleChange} />
+            <button className='input'>
+                Save
+            </button>
+        </form>
+    );
 }
 
 export default BookEdit;
